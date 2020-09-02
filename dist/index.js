548bc64cb93a80ba20108d5fd042bbeeff482319
tran <anltnm93@gmail.com>
2020-09-02 10:05:01 +1000

    feat(doc-v3): Add missing dependencies

--- dist/index.js
+++ dist/index.js
@@ -11,11 +11,67 @@ Object.defineProperty(exports, "WinterfellFormBuilderReducer", {
     return _winterfellFormBuilderReducer["default"];
   }
 });
+Object.defineProperty(exports, "addQuestion", {
+  enumerable: true,
+  get: function get() {
+    return _winterfellFormBuilderActions.addQuestion;
+  }
+});
+Object.defineProperty(exports, "addQuestionSet", {
+  enumerable: true,
+  get: function get() {
+    return _winterfellFormBuilderActions.addQuestionSet;
+  }
+});
+Object.defineProperty(exports, "goToPage", {
+  enumerable: true,
+  get: function get() {
+    return _winterfellFormBuilderActions.goToPage;
+  }
+});
+Object.defineProperty(exports, "onSelectNextButtonAction", {
+  enumerable: true,
+  get: function get() {
+    return _winterfellFormBuilderActions.onSelectNextButtonAction;
+  }
+});
+Object.defineProperty(exports, "onSelectNextButtonTarget", {
+  enumerable: true,
+  get: function get() {
+    return _winterfellFormBuilderActions.onSelectNextButtonTarget;
+  }
+});
+Object.defineProperty(exports, "addPage", {
+  enumerable: true,
+  get: function get() {
+    return _winterfellFormBuilderActions.addPage;
+  }
+});
+Object.defineProperty(exports, "changeCurrentEditingField", {
+  enumerable: true,
+  get: function get() {
+    return _winterfellFormBuilderActions.changeCurrentEditingField;
+  }
+});
+Object.defineProperty(exports, "deletePage", {
+  enumerable: true,
+  get: function get() {
+    return _winterfellFormBuilderActions.deletePage;
+  }
+});
+Object.defineProperty(exports, "createForm", {
+  enumerable: true,
+  get: function get() {
+    return _winterfellFormBuilderActions.createForm;
+  }
+});
 exports["default"] = void 0;
 
 var _WinterfellFormBuilder = _interopRequireDefault(require("./components/WinterfellFormBuilder"));
 
 var _winterfellFormBuilderReducer = _interopRequireDefault(require("./reducers/winterfellFormBuilderReducer"));
+
+var _winterfellFormBuilderActions = require("./actions/winterfellFormBuilderActions");
 
 var _default = _WinterfellFormBuilder["default"];
 exports["default"] = _default;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-import WinterfellFormBuilder from './components/WinterfellFormBuilder';
-import WinterfellFormBuilderReducer from './reducers/winterfellFormBuilderReducer';
+import WinterfellFormBuilder from "./components/WinterfellFormBuilder";
+import WinterfellFormBuilderReducer from "./reducers/winterfellFormBuilderReducer";
+import {
+  addQuestion,
+  addQuestionSet,
+  goToPage,
+  onSelectNextButtonAction,
+  onSelectNextButtonTarget,
+  addPage,
+  changeCurrentEditingField,
+  deletePage,
+  createForm,
+} from "./actions/winterfellFormBuilderActions";
 
-export { WinterfellFormBuilderReducer };
+export {
+  WinterfellFormBuilderReducer,
+  addQuestion,
+  addQuestionSet,
+  goToPage,
+  onSelectNextButtonAction,
+  onSelectNextButtonTarget,
+  addPage,
+  changeCurrentEditingField,
+  deletePage,
+  createForm,
+};
 export default WinterfellFormBuilder;
-
