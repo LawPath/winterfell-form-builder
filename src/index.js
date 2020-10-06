055ff7b1e33c2d6a8b28348e08ca5990f8d40c89
tran <anltnm93@gmail.com>
2020-10-07 10:23:15 +1100

    feat(document-editor): Remove export redux actions

--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
 import WinterfellFormBuilder from './components/WinterfellFormBuilder';
 import WinterfellFormBuilderReducer from './reducers/winterfellFormBuilderReducer';
-// import {
-//   addQuestion,
-//   addQuestionSet,
-//   goToPage,
-//   onSelectNextButtonAction,
-//   onSelectNextButtonTarget,
-//   addPage,
-//   changeCurrentEditingField,
-//   deletePage,
-//   createForm,
-//   editNextButtonText,
-// } from './actions/winterfellFormBuilderActions';
 
-// export {
-//   WinterfellFormBuilderReducer,
-//   addQuestion,
-//   addQuestionSet,
-//   goToPage,
-//   onSelectNextButtonAction,
-//   onSelectNextButtonTarget,
-//   addPage,
-//   changeCurrentEditingField,
-//   deletePage,
-//   createForm,
-//   editNextButtonText,
-// };
 export { WinterfellFormBuilderReducer };
-
 export default WinterfellFormBuilder;
