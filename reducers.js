2bf5390a95e0ac13cc3ce672310faed8dcf30742
tran <anltnm93@gmail.com>
2020-10-07 10:05:38 +1100

    feat(document-editor): Remove export redux actions

--- reducers.js
+++ reducers.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux-immutable';
 
 import formReducer from './src/reducers/winterfellFormBuilderReducer';
 import errorReducer from './src/reducers/winterfellErrorReducer';
-import tabReducer from './src/reducers/winterfellFormBuilderTabReducer';
 
 /**
  * Creates the main reducer with the asynchronously loaded ones
@@ -11,7 +10,6 @@ export default function createReducer(asyncReducers) {
   return combineReducers({
     form: formReducer,
     error: errorReducer,
-    formBuilderTabs: tabReducer,
     ...asyncReducers,
   });
 }
